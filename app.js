dfe7083a6d018a859d276c66e6ac1c7c524e01d6
cybor97 <muhametshin.1997@gmail.com>
2019-07-28 00:32:55 +0300

    Added temporary cors stub

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ const app = express();
 app
     .use(express.json())
     .use(express.urlencoded({ extended: true }))
+    .use((req, res, next) => {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        next();
+    })
     .use('/api', auth)
     //.use(user)
     .listen(8081, 'localhost');
