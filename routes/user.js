0d13f786de2f1d3a0b3e4fe7f3a25d6970d5fe1e
cybor97 <muhametshin.1997@gmail.com>
2019-07-31 01:02:01 +0300

    Added nickname generation by firstname and lastname

--- routes/user.js
+++ routes/user.js
@@ -11,6 +11,9 @@ router
             : { snsId: req.snsId, snsName: req.snsName };
         let user = await User.findOne({ where: params });
         params = Object.assign(req.body, params);
+        if (params.firstname || params.lastname) {
+            params.nickname = [params.firstname || '', params.lastname || ''].join(' ').trim();
+        }
         let result = user ? user.dataValues : {};
         if (user) {
             await user.update(params);
